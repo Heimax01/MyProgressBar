97c7d09002d0dcd25824e080f64d965368e23d63
Heitor Cordeiro <heitor.cordeiro.dev@gmail.com>
2022-01-15 19:12:46 -0500

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,26 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Title</title>
+        <link rel="shortcut icon" href="/img/" />
+        <link rel="apple-touch-icon" href="/img/" />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        ></link>
+        <meta name="description" content="My Projects" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
